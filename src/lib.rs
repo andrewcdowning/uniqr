b2717b7ccbdacc77b656189eee0177e098594a40
Andrew Downing <andrew.c.downing@gmail.com>
2023-04-26 20:49:03 -0600

    feat: count and out file added

---
+++ src/lib.rs
@@ -0,0 +1,93 @@
+use clap::{App, Arg};
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead, BufReader, Write},
+};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    in_file: String,
+    out_file: Option<String>,
+    count: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("uniqr")
+        .version("0.1.0")
+        .author("me")
+        .about("Rust uniq")
+        .arg(
+            Arg::with_name("in_file")
+                .value_name("IN_FILE")
+                .help("Input file")
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("out_file")
+                .value_name("OUT_FILE")
+                .help("Output file"),
+        )
+        .arg(
+            Arg::with_name("count")
+                .short("c")
+                .long("count")
+                .help("Shows count")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    Ok(Config {
+        in_file: matches.value_of_lossy("in_file").unwrap().to_string(),
+        out_file: matches.value_of_lossy("out_file").map(|v| v.to_string()),
+        count: matches.is_present("count"),
+    })
+}
+
+pub fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    let mut file = open(&config.in_file).map_err(|e| format!("{}: {}", config.in_file, e))?;
+
+    let mut out_file: Box<dyn Write> = match &config.out_file {
+        Some(out_name) => Box::new(File::create(out_name)?),
+        _ => Box::new(io::stdout()),
+    };
+    let mut previous = String::new();
+    let mut lines = String::new();
+    let mut count: u64 = 0;
+
+    let mut print = |count: u64, text: &str| -> MyResult<()> {
+        if count > 0 {
+            if config.count {
+                write!(out_file, "{:>4} {}", count, text);
+            } else {
+                write!(out_file, "{}", text);
+            }
+        };
+        Ok(())
+    };
+
+    loop {
+        let bytes = file.read_line(&mut lines)?;
+        if bytes == 0 {
+            break;
+        }
+        if lines.trim_end() != previous.trim_end() {
+            print(count, &previous);
+            previous = lines.clone();
+            count = 0;
+        }
+        count += 1;
+        lines.clear();
+    }
+    print(count, &previous);
+    Ok(())
+}
